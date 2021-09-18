f467e290cfd0e3de2d0defd4135f201f4db271ad
denglexi <476631901@qq.com>
2021-09-18 17:52:34 +0800

    first commit

---
+++ dlx-generator/src/main/java/com/generator/dao/OracleGeneratorDao.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ *
+ * 
+ *
+ * 
+ */
+
+package com.generator.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * Oracle代码生成器
+ *
+ * @author Mark 
+ */
+@Mapper
+public interface OracleGeneratorDao extends GeneratorDao {
+
+}
---
+++ dlx-generator/src/main/java/com/generator/dao/PostgreSQLGeneratorDao.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ *
+ * 
+ *
+ * 
+ */
+
+package com.generator.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * PostgreSQL代码生成器
+ *
+ * @author Mark 
+ */
+@Mapper
+public interface PostgreSQLGeneratorDao extends GeneratorDao {
+
+}
